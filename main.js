991846cc8ff00637da8f637a6cf767e3a207fa74
Hussein <eng.anabtawi@gmail.com>
2022-12-30 00:20:07 +0200

    Added Edit Functionality

--- main.js
+++ main.js
@@ -15,6 +15,10 @@ const timeError = document.getElementById("timeError")
 
 const cardsContainer = document.getElementById("cardscontainer")
 
+const formTitle = document.getElementById("formTitle")
+const buttonAdd = document.getElementById("buttonAdd")
+const noteid = document.getElementById("noteidhidden")
+
 websiteInit();
 
 function websiteInit() {
@@ -25,12 +29,17 @@ function websiteInit() {
     else {
         Notes = []
     }
+
     console.log("Website Initilize...")
     console.log(Notes)
+
+    //AddDemoData()
+
     drawCards();
 }
 
 function GetFormValues() {
+
 
     let note = {
         id: Id++,
@@ -38,12 +47,48 @@ function GetFormValues() {
         description: noteDesciptionId.value,
         date: dueDateId.value,
         time: dueTimeId.value,
-        checked : false,
-       
+        checked: false,
+        inserttime: GetCurDate(),
+        ineditmode: false,
     }
 
     return note
 }
+
+function getDateFromInput(strDate)
+{
+    const [year, month, day] = strDate.split('-');
+    const date = new Date(+year, month - 1, +day+1);
+
+    return date.toISOString().split('T')[0] 
+}
+
+function UpdateNote() {
+    let id = noteid.innerHTML
+    let note = FindNoteOfId(id)
+    let noteIndex = Notes.indexOf(note)
+    if (note != "") {
+        note.note = noteId.value
+        note.description = noteDesciptionId.value
+        note.date = dueDateId.value;
+        note.time = dueTimeId.value
+        note.checked = false
+        note.inserttime = GetCurDate()
+        note.ineditmode = false
+
+        noteid.innerHTML = ""
+        formTitle.innerHTML = "Add Task"
+        buttonAdd.innerHTML = "Add"
+
+        Notes[Notes] = note
+
+        const stringifyProducts = JSON.stringify(Notes)
+        localStorage.setItem("notes", stringifyProducts)
+        drawCards()
+        ClearNotes()
+    }
+}
+
 
 
 function ClearNotes() {
@@ -55,6 +100,10 @@ function ClearNotes() {
     dateError.innerHTML = ""
     dueTimeId.value = ""
     timeError.innerHTML = ""
+
+    formTitle.innerHTML = "Add Task"
+    buttonAdd.innerHTML = "Add"
+
 }
 
 function AddNotesToLocalStorage() {
@@ -75,7 +124,12 @@ function AddNotesToLocalStorage() {
 
 
 function AddNoteClick() {
-    AddNotesToLocalStorage()
+    let id = noteid.innerHTML
+
+    if (id == "")
+        AddNotesToLocalStorage()
+    else
+        UpdateNote()
 }
 
 
@@ -86,39 +140,277 @@ function validationErrors(obj) {
     if (!obj.date) dateError.innerHTML = "Please Fill Due Date";
     if (!obj.time) timeError.innerHTML = "Please Fill Due Time";
 
-    if (!obj.note || !obj.description || !obj.date || !obj.time )
+    if (!obj.note || !obj.description || !obj.date || !obj.time)
         return false
 
     return true
 }
 
 
+function drawCards() {
+    cardsContainer.innerHTML = ""
 
-
-function drawCards()
-{
-    for(const item of Notes)
-    {
-        console.log(item)
+    for (const item of Notes) {
         const cardBox = document.createElement("div")
-        const cardTitle = document.createElement("h2")
+        if (item.checked)
+            cardBox.setAttribute("class", "card myDIV cardChecked")
+        else
+            cardBox.setAttribute("class", "card myDIV")
+
+
+        const cardBody = document.createElement("div")
+        cardBody.setAttribute("class", "card-body")
+
+        const cardTitle = document.createElement("div")
+        cardTitle.setAttribute("class", "cardtitle btndiv")
+
+        const cardTitleP = document.createElement("p")
+        if (item.checked)
+            cardTitleP.setAttribute("class", "cardName titleCheked")
+        else
+            cardTitleP.setAttribute("class", "cardName")
+
+        cardTitleP.innerHTML = item.note
+
+        // const toolTipSpan  = document.createElement("span")
+        // toolTipSpan.setAttribute("class","tooltiptext")
+        // toolTipSpan.innerHTML = item.note
+
+        // cardTitleP.appendChild(toolTipSpan)
+
+        const cardTitleButton = document.createElement("button")
+        cardTitleButton.setAttribute("class", "hide")
+        cardTitleButton.innerText = "X"
+        cardTitleButton.addEventListener("click", DeleteNote, false)
+        cardTitleButton.myId = item.id;
+
+        const cardTitleBr = document.createElement("br")
+
+        cardTitle.appendChild(cardTitleP)
+        cardTitle.appendChild(cardTitleButton)
+
         const cardDescription = document.createElement("p")
-        const cardPrice = document.createElement("p")
-        const cardDiscount = document.createElement("p")
+        if (item.checked)
+            cardDescription.setAttribute("class", "card-text titleCheked")
+        else
+            cardDescription.setAttribute("class", "card-text")
+
+        cardDescription.innerHTML = item.description
+
+        const cardInsertTimeP = document.createElement("p")
+        if (item.checked)
+            cardInsertTimeP.setAttribute("class", "card-text titleCheked")
+        else
+            cardInsertTimeP.setAttribute("class", "card-text")
 
 
-        cardTitle.innerHTML = item.note;
-        cardDescription.innerHTML = item.description;
-        cardPrice.innerHTML = item.date;
-        cardDiscount.innerHTML = item.time.replace(":","-");
+        let newDate = new Date(item.date + " @ " +item.time)
+        cardInsertTimeP.innerHTML = "Due Date : " + FormateDate(newDate)
 
-        cardBox.appendChild(cardTitle)
-        cardBox.appendChild(cardDescription)
-        cardBox.appendChild(cardPrice)
-        cardBox.appendChild(cardDiscount)
+        const cardDueDateP = document.createElement("p")
+        if (item.checked)
+            cardDueDateP.setAttribute("class", "inserttimetext titleCheked")
+        else
+            cardDueDateP.setAttribute("class", "inserttimetext")
 
+
+        cardDueDateP.innerHTML = item.inserttime;
+
+        const cardFooter = document.createElement("div")
+        cardFooter.setAttribute("class", "footer-button")
+
+        const cardFooterBtn1 = document.createElement("button")
+        cardFooterBtn1.setAttribute("class", "btn btn-secondary")
+        cardFooterBtn1.innerHTML = "Edit"
+        cardFooterBtn1.addEventListener("click", EditNote, false)
+        cardFooterBtn1.myId = item.id;
+
+        const cardFooterBtn2 = document.createElement("button")
+        cardFooterBtn2.setAttribute("class", "btn btn-secondary")
+        cardFooterBtn2.innerHTML = item.checked ? "Re-Add" : "Done"
+        cardFooterBtn2.addEventListener("click", DoneNote, false)
+        cardFooterBtn2.myId = item.id;
+
+
+
+        cardFooter.appendChild(cardFooterBtn1)
+        cardFooter.appendChild(cardFooterBtn2)
+
+
+        cardBody.appendChild(cardTitle)
+        cardBody.appendChild(cardTitleBr)
+        cardBody.appendChild(cardDescription)
+        cardBody.appendChild(cardInsertTimeP)
+        cardBody.appendChild(cardFooter)
+        cardBody.appendChild(cardDueDateP)
+
+
+
+        cardBox.appendChild(cardBody)
 
         cardsContainer.appendChild(cardBox)
 
     }
+}
+
+function DeleteNote(evt) {
+    let note = FindNoteOfId(evt.currentTarget.myId)
+    if (note != 0) {
+        let index = Notes.indexOf(note)
+        Notes.splice(index, 1)
+    }
+    const stringifyProducts = JSON.stringify(Notes)
+    localStorage.setItem("notes", stringifyProducts)
+    drawCards()
+}
+function EditNote(evt) {
+    let note = FindNoteOfId(evt.currentTarget.myId)
+    if (note != 0) {
+        note.ineditmode = true;
+        FillForm(note)
+    }
+}
+
+function FillForm(note) {
+    noteId.value = note.note
+    noteError.innerHTML = ""
+    noteDesciptionId.value = note.description
+    descriptionError.innerHTML = ""
+
+    // strDate = note.date.toISOString().split('T')[0]
+
+    // const [year, month, day] = strDate.split('-');
+    // const date = new Date(+year, month - 1, +day+1);
+
+
+    dueDateId.value = note.date//date.toISOString().split('T')[0]
+
+    dateError.innerHTML = ""
+    dueTimeId.value = note.time
+    timeError.innerHTML = ""
+    noteid.innerHTML = note.id
+
+
+    formTitle.innerHTML = "Edit Task"
+    buttonAdd.innerHTML = "Update"
+}
+
+function DoneNote(evt) {
+    let note = FindNoteOfId(evt.currentTarget.myId)
+    if (note != 0) {
+        note.checked = !note.checked
+    }
+    const stringifyProducts = JSON.stringify(Notes)
+    localStorage.setItem("notes", stringifyProducts)
+    drawCards()
+
+}
+
+function FindNoteOfId(id) {
+    for (const n of Notes)
+        if (n.id == id)
+            return n
+
+    return 0
+}
+
+function GetCurDate() {
+    let currentdate = new Date();
+    return "Added At: " + FormateDate(currentdate);
+}
+
+
+function FormateDate(currentdate) {
+    let datetime = currentdate.getDate() + "/"
+        + (currentdate.getMonth() + 1) + "/"
+        + currentdate.getFullYear() + " @ "
+        + currentdate.getHours() + ":"
+        + currentdate.getMinutes() + ":"
+        + currentdate.getSeconds();
+    return datetime;
+}
+
+function AddDemoData() {
+    Notes = [];
+    Id = 0;
+
+    let note = {
+        id: Id++,
+        note: "Get Milk",
+        description: "Two Cartons 2 litter",
+        date: "2022-12-31" ,
+        time: "12:00",
+        checked: true,
+        inserttime: GetCurDate(),
+    }
+
+    Notes.push(note)
+
+    note = {
+        id: Id++,
+        note: "Fix Car",
+        description: "Mandatory",
+        date: "2023-01-15" ,
+        time: "10:10",
+        checked: false,
+        inserttime: GetCurDate(),
+    }
+
+    Notes.push(note)
+
+    note = {
+        id: Id++,
+        note: "Pick Kids",
+        description: "School",
+        date: "2022-10-29",
+        time: "02:30",
+        checked: true,
+        inserttime: GetCurDate(),
+    }
+
+    Notes.push(note)
+
+
+    note = {
+        id: Id++,
+        note: "Doctor Appointment",
+        description: "Back Pain Check",
+        date: "2023-03-01",
+        time: "09:30",
+        checked: false,
+        inserttime: GetCurDate(),
+    }
+
+    Notes.push(note)
+
+    note = {
+        id: Id++,
+        note: "Mom Birthday",
+        description: "Get Gift and Flowers",
+        date: "2023-04-01",
+        time: "08:00",
+        checked: false,
+        inserttime: GetCurDate(),
+    }
+
+    Notes.push(note)
+
+    note = {
+        id: Id++,
+        note: "Aniversary",
+        description: "Get Gift and Flowers",
+        date: "2023-02-14",
+        time: "10:00",
+        checked: false,
+        inserttime: GetCurDate(),
+    }
+
+    Notes.push(note)
+
+
+    const stringifyProducts = JSON.stringify(Notes)
+    localStorage.setItem("notes", stringifyProducts)
+    ClearNotes()
+    drawCards()
+
 }
